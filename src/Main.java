0cade94602e65bad1406d1af105d70717bf59632
Mehmet Ali Tezcan <mehmetalitez33@gmail.com>
2025-03-12 23:22:43 +0300

    ilk

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+//Sayı Yuvarlama (OPSİYONEL)
+//Kullanıcının girdiği ondalıklı bir sayıyı aşağı, yukarı ve en yakın tam sayıya yuvarlayarak ekrana yazdıran kodu yazma
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner =new Scanner(System.in);
+        double ondalikli_sayi;
+        while (true){
+            System.out.println("Ondalıklı sayı giriniz: ");
+            if(scanner.hasNextDouble()){
+                ondalikli_sayi= scanner.nextDouble();
+                System.out.println("---------Geçerli----------");
+                int asagiyuvarla=(int)Math.floor(ondalikli_sayi);
+                int yukariyuvarla=(int)Math.ceil(ondalikli_sayi);
+                int enyakintamsayi=(int)Math.round(ondalikli_sayi);
+                System.out.println("Aşağı Yuvarlama : "+ asagiyuvarla);
+                System.out.println("Yukarı Yuvarlama : " + yukariyuvarla);
+                System.out.println("En Yakın Tam Sayı : " + enyakintamsayi);
+                break;
+            }else {
+                System.out.println("Hata : Lütfen ondalıklı sayı giriniz!");
+                scanner.next();
+            }
         }
     }
 }
